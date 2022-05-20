5d8de96e7334aac512932dbf1840e56fdd3f6981
matthewgani <53487361+matthewgani@users.noreply.github.com>
2022-05-20 19:17:50 +0800

    Finish 1-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  // let converted = ((temp - 32) * (5/9));
+  // return Math.round((converted* 10 )/10 , 1);
+  // wrong bc the /10 is outside of the math.round
 
+
+  return Math.round((f - 32) * (5/9) * 10) / 10;
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  // let converted = ((temp*(9/5)) +32);
+  // return Math.round((converted * 10) / 10 , 1);
+  return Math.round(((c * 9/5) + 32) * 10) / 10;
 
 };
 
